59517333cf082a6638b9094dd25873b43943917e
adapa <adapasai008@gmail.com>
2026-03-24 20:45:16 +0530

    26 Solution update with better approach.

--- adapaPractice/src/leetCode/easy/RemoveDuplicatesFromSortedArry_26.java
+++ adapaPractice/src/leetCode/easy/RemoveDuplicatesFromSortedArry_26.java
@@ -3,28 +3,23 @@ package leetCode.easy;
 public class RemoveDuplicatesFromSortedArry_26 {
 
 	public static int removeDuplicates(int[] nums) {
-		int start = 0;
-		int travel = 1;
-		int answer = 1;
+		int result = 0;
 
-		//Two pointers approach.
-		while (travel < nums.length) {
-			if (nums[start] != nums[travel]) {
-				nums[answer] = nums[travel];
-				start = travel;
-				answer++;
+		for (int i = 0; i < nums.length; i++) {
 
+			if (nums[result] != nums[i]) {// here we are comparing with updated array to the old array
+				result++;
+				nums[result] = nums[i];
 			}
-			travel++;
 
 		}
 
-		return answer;
+		return result + 1; // here we are updating the index value to the count.
 	}
 
 	public static void main(String[] args) {
 
-		int[] nums = { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
+		int[] nums = { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4, 7, 7, 9 };
 		int result = removeDuplicates(nums);
 
 		for (int i = 0; i < result; i++) {
