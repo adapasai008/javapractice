7d8357636cb8a1c6b95ed5774b04906319aa71b2
adapa <adapasai008@gmail.com>
2026-04-03 18:56:46 +0530

    3Sum : Brute force solution

--- adapaPractice/src/leetCode/medium/t3Sum_15.java
+++ adapaPractice/src/leetCode/medium/t3Sum_15.java
@@ -60,3 +60,18 @@ public class t3Sum_15 {
 	}
 
 }
+
+/*
+ * //Brute force solution 
+ * public static List<List<Integer>> threesum(int[] nums)
+ * { Set<List<Integer>> result = new HashSet<List<Integer>>();
+ * 
+ * for (int i = 0; i < nums.length; i++) { for (int j = i + 1; j < nums.length;
+ * j++) { for (int k = j + 1; k < nums.length; k++) {
+ * 
+ * if ((nums[i] + nums[j] + nums[k]) == 0) { List<Integer> triplets =
+ * Arrays.asList(nums[i], nums[j], nums[k]); Collections.sort(triplets);
+ * result.add(triplets); } } } }
+ * 
+ * return new ArrayList<>(result); }
+ */
